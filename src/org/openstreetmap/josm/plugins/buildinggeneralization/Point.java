5b9fa8e3f8df37f68f90cdc76abe4bffa0fac096
Vincent Privat <vincent.privat@gmail.com>
2016-06-16 04:36:25 +0200

    fix crash with JOSM >= 10358 + code cleanup

--- src/org/openstreetmap/josm/plugins/buildinggeneralization/Point.java
+++ src/org/openstreetmap/josm/plugins/buildinggeneralization/Point.java
@@ -1,55 +1,42 @@
+// License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.buildinggeneralization;
 public class Point {
-	
-	double x;
-	double y;
-	
-	public Point(){
-		this.x = 0;
-		this.y = 0;
-	}
-	public Point(double x, double y){
-		this.x = x;
-		this.y = y;
-	}
-	
-//	public static Point project(Point x1, Point x2, Point toProject)
-//	{
-//	    double m = (x2.y - x1.y) / (x2.x - x1.x);
-//	    double b = x1.y - (m * x1.y);
-//	 
-//	    double x = (m * toProject.y + toProject.x - m * b) / (m * m + 1);
-//	    double y = (m * m * toProject.y + m * toProject.x + b) / (m * m + 1);
-//	 
-//	    return new Point(x,y);
-//	}
-	
-	public static double distance(Point x1, Point x2){
-		
-		return Math.sqrt(Math.pow(x2.x - x1.x, 2) + Math.pow(x2.y - x1.y, 2)); // always positive
-		
-	}
-	
-	public static double distance(Point lineX1, Point lineX2, Point p){
-		double A = p.x - lineX1.x;
-		double B = p.y - lineX1.y;
-		double C = lineX2.x - lineX1.x;
-		double D = lineX2.y - lineX1.y;
-		
-		double dot = A * C + B * D;
-		double len_sq = C*C + D*D;
-		double param = dot/len_sq;
-		double xx = lineX1.x + param * C;
-		double yy = lineX1.y + param * D;
-		double dx = p.x - xx;
-		double dy = p.y - yy;
-		return Math.sqrt(dx * dx + dy * dy);
-		
-	}
-	
-	@Override
-	public String toString() {
-		return "Point [x=" + x + ", y=" + y + "]";
-	}
-	
+    
+    double x;
+    double y;
+    
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+    
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public static double distance(Point x1, Point x2){
+        return Math.sqrt(Math.pow(x2.x - x1.x, 2) + Math.pow(x2.y - x1.y, 2)); // always positive
+    }
+    
+    public static double distance(Point lineX1, Point lineX2, Point p) {
+        double A = p.x - lineX1.x;
+        double B = p.y - lineX1.y;
+        double C = lineX2.x - lineX1.x;
+        double D = lineX2.y - lineX1.y;
+        
+        double dot = A * C + B * D;
+        double len_sq = C*C + D*D;
+        double param = dot/len_sq;
+        double xx = lineX1.x + param * C;
+        double yy = lineX1.y + param * D;
+        double dx = p.x - xx;
+        double dy = p.y - yy;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    
+    @Override
+    public String toString() {
+        return "Point [x=" + x + ", y=" + y + ']';
+    }
 }
--- src/org/openstreetmap/josm/plugins/buildinggeneralization/ShapeMath.java
+++ src/org/openstreetmap/josm/plugins/buildinggeneralization/ShapeMath.java
@@ -1,3 +1,4 @@
+// License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.buildinggeneralization;
 
 import java.util.Collection;
@@ -15,224 +16,211 @@ import org.openstreetmap.josm.data.osm.WaySegment;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 
 public class ShapeMath {
-	 static Way containingWay;
-	/**
-	 * 
-	 * @param building
-	 * @param roadSegment
-	 * @return
-	 */
-	public static WaySegment getClosestSegment(Way building, WaySegment roadSegment){
-		double maxDistance = Double.MAX_VALUE;
-		WaySegment closestSegment = null;
-	
-		for(int i=0;i<building.getNodesCount()-1;i++){
+    static Way containingWay;
 
-			WaySegment currentSegment = new WaySegment(building, i);
-			EastNorth currentSegmentCentroid = getCentroid(currentSegment);
-			Point p = new Point(currentSegmentCentroid.getX(), currentSegmentCentroid.getY());
-			Point x1 = new Point(roadSegment.getFirstNode().getEastNorth().getX(), roadSegment.getFirstNode().getEastNorth().getY());
-			Point x2 = new Point(roadSegment.getSecondNode().getEastNorth().getX(), roadSegment.getSecondNode().getEastNorth().getY());
-			double distance = Point.distance(x1,x2,p);
-			
-			if(distance < maxDistance){
-				maxDistance = distance;
-				closestSegment = new WaySegment(building,i);
-			}
-		}
-		return closestSegment;
-	}
-	
-	public static String formatSegment(WaySegment segment){
-		String r = "\n Segment \n firstNode: x=";
-		
-		r += segment.getFirstNode().getCoor().getX() + ", y=";
-		r += segment.getFirstNode().getCoor().getY();
-		r += "\n secondNode: x=";
-		r += segment.getSecondNode().getCoor().getX() + ", y=";
-		r += segment.getSecondNode().getCoor().getY();
-		return r;
-	}
-	
-	public static void rotate(Node node, double angle, EastNorth center) {
-		EastNorth oldCoor = node.getEastNorth();
-		node.setEastNorth(getRotation(oldCoor, angle, center));
-	}
+    public static WaySegment getClosestSegment(Way building, WaySegment roadSegment){
+        double maxDistance = Double.MAX_VALUE;
+        WaySegment closestSegment = null;
+    
+        for(int i=0;i<building.getNodesCount()-1;i++){
 
-	public static void rotate(Way way, double angle, EastNorth center) {
-		Set<Node> nodesSet = new HashSet<Node>();
-		List<Node> wayNodes = way.getNodes();
-		for (Node node : wayNodes) {
-			nodesSet.add(node);
-		}
-		EastNorth allNodesCenter = ShapeMath.getCentroid(nodesSet);
-		Iterator<Node> it = nodesSet.iterator();
-		while(it.hasNext()){
-			rotate(it.next(), angle, allNodesCenter);
-		}
-		Main.map.repaint();
-	}
+            WaySegment currentSegment = new WaySegment(building, i);
+            EastNorth currentSegmentCentroid = getCentroid(currentSegment);
+            Point p = new Point(currentSegmentCentroid.getX(), currentSegmentCentroid.getY());
+            Point x1 = new Point(roadSegment.getFirstNode().getEastNorth().getX(), roadSegment.getFirstNode().getEastNorth().getY());
+            Point x2 = new Point(roadSegment.getSecondNode().getEastNorth().getX(), roadSegment.getSecondNode().getEastNorth().getY());
+            double distance = Point.distance(x1,x2,p);
+            
+            if(distance < maxDistance){
+                maxDistance = distance;
+                closestSegment = new WaySegment(building,i);
+            }
+        }
+        return closestSegment;
+    }
+    
+    public static String formatSegment(WaySegment segment){
+        String r = "\n Segment \n firstNode: x=";
+        
+        r += segment.getFirstNode().getCoor().getX() + ", y=";
+        r += segment.getFirstNode().getCoor().getY();
+        r += "\n secondNode: x=";
+        r += segment.getSecondNode().getCoor().getX() + ", y=";
+        r += segment.getSecondNode().getCoor().getY();
+        return r;
+    }
+    
+    public static void rotate(Node node, double angle, EastNorth center) {
+        node.setEastNorth(getRotation(node.getEastNorth(), angle, center));
+    }
 
-	public static EastNorth getRotation(EastNorth originalPoint, double angle,
-			EastNorth center) {
-		double x = Math.cos(angle) * (originalPoint.getX() - center.getX())
-				- Math.sin(angle) * (originalPoint.getY() - center.getY())
-				+ center.getX();
-		double y = Math.sin(angle) * (originalPoint.getX() - center.getX())
-				+ Math.cos(angle) * (originalPoint.getY() - center.getY())
-				+ center.getY();
-		return new EastNorth(x, y);
-	}
+    public static void rotate(Way way, double angle, EastNorth center) {
+        Set<Node> nodesSet = new HashSet<>();
+        List<Node> wayNodes = way.getNodes();
+        for (Node node : wayNodes) {
+            nodesSet.add(node);
+        }
+        EastNorth allNodesCenter = ShapeMath.getCentroid(nodesSet);
+        Iterator<Node> it = nodesSet.iterator();
+        while(it.hasNext()){
+            rotate(it.next(), angle, allNodesCenter);
+        }
+        Main.map.repaint();
+    }
 
-	public static EastNorth getCentroid(Way way) {
-		double x = 0, y = 0;
-		List<Node> wayNodes = way.getNodes();
+    public static EastNorth getRotation(EastNorth originalPoint, double angle, EastNorth center) {
+        double x = Math.cos(angle) * (originalPoint.getX() - center.getX())
+                 - Math.sin(angle) * (originalPoint.getY() - center.getY())
+                 + center.getX();
+        double y = Math.sin(angle) * (originalPoint.getX() - center.getX())
+                 + Math.cos(angle) * (originalPoint.getY() - center.getY())
+                 + center.getY();
+        return new EastNorth(x, y);
+    }
 
-		if (way.isClosed()) {
-			for (int i = 0; i < wayNodes.size() - 1; i++) {
-				x += wayNodes.get(i).getEastNorth().getX();
-				y += wayNodes.get(i).getEastNorth().getY();
-			}
-			x = x / (way.getNodesCount() - 1);
-			y = y / (way.getNodesCount() - 1);
-			return new EastNorth(x, y);
-		}
+    public static EastNorth getCentroid(Way way) {
+        double x = 0, y = 0;
+        List<Node> wayNodes = way.getNodes();
 
-		for (int i = 0; i < wayNodes.size(); i++) {
-			x += wayNodes.get(i).getEastNorth().getX();
-			y += wayNodes.get(i).getEastNorth().getY();
-		}
-		x = x / way.getNodesCount();
-		y = y / way.getNodesCount();
+        if (way.isClosed()) {
+            for (int i = 0; i < wayNodes.size() - 1; i++) {
+                x += wayNodes.get(i).getEastNorth().getX();
+                y += wayNodes.get(i).getEastNorth().getY();
+            }
+            x = x / (way.getNodesCount() - 1);
+            y = y / (way.getNodesCount() - 1);
+            return new EastNorth(x, y);
+        }
 
-		return new EastNorth(x, y);
-	}
-	
-	public static EastNorth getCentroid(WaySegment segment){
-		double x = 0, y = 0;
-		
-		x = x + segment.getFirstNode().getEastNorth().getX() + segment.getSecondNode().getEastNorth().getX();
-		y = y + segment.getFirstNode().getEastNorth().getY() + segment.getSecondNode().getEastNorth().getY();
-		
-		x = x / 2.0;
-		y = y / 2.0;
-		
-		return new EastNorth(x,y);
-	}
-	
-	public static EastNorth getCentroid(List<Way> wayList) {
-		double x = 0, y = 0;
-		for (int i = 0; i < wayList.size(); i++) {
-			EastNorth currentCenter = getCentroid(wayList.get(i));
-			x += currentCenter.getX();
-			y += currentCenter.getY();
-		}
-		x = x / wayList.size();
-		y = y / wayList.size();
-		return new EastNorth(x, y);
-	}
+        for (int i = 0; i < wayNodes.size(); i++) {
+            x += wayNodes.get(i).getEastNorth().getX();
+            y += wayNodes.get(i).getEastNorth().getY();
+        }
+        x = x / way.getNodesCount();
+        y = y / way.getNodesCount();
 
-	public static EastNorth getCentroid(Set<Node> nodes) {
-		Iterator<Node> i = nodes.iterator();
-		double x = 0, y = 0;
-		while (i.hasNext()) {
-			Node currentNode = i.next();
-			x += currentNode.getEastNorth().getX();
-			y += currentNode.getEastNorth().getY();
-		}
-		x = x / nodes.size();
-		y = y / nodes.size();
-		return new EastNorth(x, y);
-	}
+        return new EastNorth(x, y);
+    }
 
-	public static void doRotate(Collection<Way> ways, Collection<Node> nodes,
-			double angle) {
-		avoidDuplicateNodesRotation(ways, nodes, angle);
-	}
+    public static EastNorth getCentroid(WaySegment segment){
+        double x = 0, y = 0;
+        
+        x = x + segment.getFirstNode().getEastNorth().getX() + segment.getSecondNode().getEastNorth().getX();
+        y = y + segment.getFirstNode().getEastNorth().getY() + segment.getSecondNode().getEastNorth().getY();
+        
+        x = x / 2.0;
+        y = y / 2.0;
+        
+        return new EastNorth(x,y);
+    }
 
-	public static void avoidDuplicateNodesRotation(Collection<Way> ways,
-			Collection<Node> nodes, double angle) {
-		System.out.println("doRotate() called: rotating shapes by: " + angle);
-		Set<Node> nodesSet = new HashSet<Node>();
-		for (Way way : ways) {
-			List<Node> wayNodes = way.getNodes();
-			for (Node node : wayNodes) {
-				nodesSet.add(node);
-			}
-		}
-		for (Node node : nodes) {
-			nodesSet.add(node);
-		}
-		EastNorth allNodesCenter = ShapeMath.getCentroid(nodesSet);
-		Iterator<Node> i = nodesSet.iterator();
-		while (i.hasNext()) {
-			rotate(i.next(), angle, allNodesCenter);
-		}
-		Main.map.repaint();
-	}
+    public static EastNorth getCentroid(List<Way> wayList) {
+        double x = 0, y = 0;
+        for (int i = 0; i < wayList.size(); i++) {
+            EastNorth currentCenter = getCentroid(wayList.get(i));
+            x += currentCenter.getX();
+            y += currentCenter.getY();
+        }
+        x = x / wayList.size();
+        y = y / wayList.size();
+        return new EastNorth(x, y);
+    }
 
-	public static void align(Way firstWay, Way secondWay) {
-		double x1 = firstWay.getNode(0).getEastNorth().getX();
-		double x2 = firstWay.getNode(1).getEastNorth().getX();
-		double x3 = secondWay.getNode(0).getEastNorth().getX();
-		double x4 = secondWay.getNode(1).getEastNorth().getX();
-		double y1 = firstWay.getNode(0).getEastNorth().getY();
-		double y2 = firstWay.getNode(1).getEastNorth().getY();
-		double y3 = secondWay.getNode(0).getEastNorth().getY();
-		double y4 = secondWay.getNode(1).getEastNorth().getY();
-		double requiredAngle = Math.atan2(y2 - y1, x2 - x1)
-				- Math.atan2(y4 - y3, x4 - x3);
-		System.out.println("Angle calculated from align() " + requiredAngle);
-		rotate(secondWay, requiredAngle, getCentroid(secondWay));
-		Main.map.repaint();
-	}
+    public static EastNorth getCentroid(Set<Node> nodes) {
+        Iterator<Node> i = nodes.iterator();
+        double x = 0, y = 0;
+        while (i.hasNext()) {
+            Node currentNode = i.next();
+            x += currentNode.getEastNorth().getX();
+            y += currentNode.getEastNorth().getY();
+        }
+        x = x / nodes.size();
+        y = y / nodes.size();
+        return new EastNorth(x, y);
+    }
 
-	public static void align(WaySegment roadSegment, WaySegment toRotateSegment) {
-		double x1 = roadSegment.getFirstNode().getEastNorth().getX();
-		double x2 = roadSegment.getSecondNode().getEastNorth().getX();
-		double x3 = toRotateSegment.getFirstNode().getEastNorth().getX();
-		double x4 = toRotateSegment.getSecondNode().getEastNorth().getX();
-		double y1 = roadSegment.getFirstNode().getEastNorth().getY();
-		double y2 = roadSegment.getSecondNode().getEastNorth().getY();
-		double y3 = toRotateSegment.getFirstNode().getEastNorth().getY();
-		double y4 = toRotateSegment.getSecondNode().getEastNorth().getY();
-		
-		double requiredAngle = Math.atan2(y2 - y1, x2 - x1)
-				- Math.atan2(y4 - y3, x4 - x3);
-		System.out.println("Angle calculated from align() " + requiredAngle);
-		
-		requiredAngle = normalise(requiredAngle);
-		
-		rotate(toRotateSegment.way, requiredAngle,
-				getCentroid(toRotateSegment.way));
-		Main.map.repaint();
-	}
-	
-	public static void align(WaySegment roadSegment, Way building){
-		WaySegment closestSegment = ShapeMath.getClosestSegment(building, roadSegment);
-		double x1 = roadSegment.getFirstNode().getEastNorth().getX();
-		double x2 = roadSegment.getSecondNode().getEastNorth().getX();
-		double x3 = closestSegment.getFirstNode().getEastNorth().getX();
-		double x4 = closestSegment.getSecondNode().getEastNorth().getX();
-		double y1 = roadSegment.getFirstNode().getEastNorth().getY();
-		double y2 = roadSegment.getSecondNode().getEastNorth().getY();
-		double y3 = closestSegment.getFirstNode().getEastNorth().getY();
-		double y4 = closestSegment.getSecondNode().getEastNorth().getY();
-		
-		double requiredAngle = Math.atan2(y2 - y1, x2 - x1)
-				- Math.atan2(y4 - y3, x4 - x3);
-		System.out.println("Angle calculated from align() " + requiredAngle);
-		
-		requiredAngle = normalise(requiredAngle);
-		
-		rotate(building, requiredAngle,
-				getCentroid(building));
-		Main.map.repaint();
-		
-	}
-	
-	
-	public static double normalise(double a){
+    public static void doRotate(Collection<Way> ways, Collection<Node> nodes, double angle) {
+        avoidDuplicateNodesRotation(ways, nodes, angle);
+    }
+
+    public static void avoidDuplicateNodesRotation(Collection<Way> ways, Collection<Node> nodes, double angle) {
+        Main.info("doRotate() called: rotating shapes by: " + angle);
+        Set<Node> nodesSet = new HashSet<>();
+        for (Way way : ways) {
+            List<Node> wayNodes = way.getNodes();
+            for (Node node : wayNodes) {
+                nodesSet.add(node);
+            }
+        }
+        for (Node node : nodes) {
+            nodesSet.add(node);
+        }
+        EastNorth allNodesCenter = ShapeMath.getCentroid(nodesSet);
+        Iterator<Node> i = nodesSet.iterator();
+        while (i.hasNext()) {
+            rotate(i.next(), angle, allNodesCenter);
+        }
+        Main.map.repaint();
+    }
+
+    public static void align(Way firstWay, Way secondWay) {
+        double x1 = firstWay.getNode(0).getEastNorth().getX();
+        double x2 = firstWay.getNode(1).getEastNorth().getX();
+        double x3 = secondWay.getNode(0).getEastNorth().getX();
+        double x4 = secondWay.getNode(1).getEastNorth().getX();
+        double y1 = firstWay.getNode(0).getEastNorth().getY();
+        double y2 = firstWay.getNode(1).getEastNorth().getY();
+        double y3 = secondWay.getNode(0).getEastNorth().getY();
+        double y4 = secondWay.getNode(1).getEastNorth().getY();
+        double requiredAngle = Math.atan2(y2 - y1, x2 - x1)
+                             - Math.atan2(y4 - y3, x4 - x3);
+        Main.info("Angle calculated from align() " + requiredAngle);
+        rotate(secondWay, requiredAngle, getCentroid(secondWay));
+        Main.map.repaint();
+    }
+
+    public static void align(WaySegment roadSegment, WaySegment toRotateSegment) {
+        double x1 = roadSegment.getFirstNode().getEastNorth().getX();
+        double x2 = roadSegment.getSecondNode().getEastNorth().getX();
+        double x3 = toRotateSegment.getFirstNode().getEastNorth().getX();
+        double x4 = toRotateSegment.getSecondNode().getEastNorth().getX();
+        double y1 = roadSegment.getFirstNode().getEastNorth().getY();
+        double y2 = roadSegment.getSecondNode().getEastNorth().getY();
+        double y3 = toRotateSegment.getFirstNode().getEastNorth().getY();
+        double y4 = toRotateSegment.getSecondNode().getEastNorth().getY();
+        
+        double requiredAngle = Math.atan2(y2 - y1, x2 - x1)
+                             - Math.atan2(y4 - y3, x4 - x3);
+        Main.info("Angle calculated from align() " + requiredAngle);
+        
+        requiredAngle = normalise(requiredAngle);
+        
+        rotate(toRotateSegment.way, requiredAngle, getCentroid(toRotateSegment.way));
+        Main.map.repaint();
+    }
+    
+    public static void align(WaySegment roadSegment, Way building){
+        WaySegment closestSegment = ShapeMath.getClosestSegment(building, roadSegment);
+        double x1 = roadSegment.getFirstNode().getEastNorth().getX();
+        double x2 = roadSegment.getSecondNode().getEastNorth().getX();
+        double x3 = closestSegment.getFirstNode().getEastNorth().getX();
+        double x4 = closestSegment.getSecondNode().getEastNorth().getX();
+        double y1 = roadSegment.getFirstNode().getEastNorth().getY();
+        double y2 = roadSegment.getSecondNode().getEastNorth().getY();
+        double y3 = closestSegment.getFirstNode().getEastNorth().getY();
+        double y4 = closestSegment.getSecondNode().getEastNorth().getY();
+        
+        double requiredAngle = Math.atan2(y2 - y1, x2 - x1)
+                             - Math.atan2(y4 - y3, x4 - x3);
+        Main.info("Angle calculated from align() " + requiredAngle);
+        
+        requiredAngle = normalise(requiredAngle);
+        
+        rotate(building, requiredAngle, getCentroid(building));
+        Main.map.repaint();
+    }
+    
+    public static double normalise(double a){
 
         while (a > Math.PI) {
             a -= 2 * Math.PI;
@@ -247,21 +235,19 @@ public class ShapeMath {
             a += Math.PI;
         }
         return a;
-	}
-	
-	public static void drawCenter() {
+    }
+    
+    public static void drawCenter() {
 
-		OsmDataLayer currentLayer = Main.main.getEditLayer();
-		DataSet data = currentLayer.data;
+        OsmDataLayer currentLayer = Main.main.getEditLayer();
+        DataSet data = currentLayer.data;
 
-		// Collection<Node> nodes = data.getSelectedNodes();
-		Collection<Way> selectedWays = data.getSelectedWays();
+        Collection<Way> selectedWays = data.getSelectedWays();
 
-		for (Way way : selectedWays) {
-			EastNorth center = ShapeMath.getCentroid(way);
-			// way.addNode(new Node(center));
-			Main.main.getEditLayer().data.addPrimitive(new Node(center));
-		}
-		Main.map.repaint();
-	}
+        for (Way way : selectedWays) {
+            EastNorth center = ShapeMath.getCentroid(way);
+            Main.main.getEditLayer().data.addPrimitive(new Node(center));
+        }
+        Main.map.repaint();
+    }
 }
